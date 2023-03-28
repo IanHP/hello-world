564e4d5381128d76f8ad9a1891b90ae33df19b9c
IanHP <ian.herzogpohl@gmail.com>
2023-03-28 12:05:20 -0400

    program turns a text file into a dictionary

--- game.py
+++ game.py
@@ -2,17 +2,123 @@
 This is a super awesome little game!!!!!
 """
 
+def setVars(wordList):
+    '''
+    puts data from a list of lists (where [i][0] = name and [i][2] = value, and each [i] is a new key:value) into dictionary
+    '''
+
+    variables = {}
+
+    for line in wordList:
+        '''
+        runs as each list in the list of lists
+        '''
+
+        #checks if list is formatted properly. If yes returns the key and value.
+        check,key,value = checkLineForKeyValue(line)
+
+        if check:
+            #then turns value into correct form.
+            value = stringToValue(value)
+
+            #adds key:value to dictionary
+            variables[key] = value
+        else:
+            print('Improperly formatted key:value pair')
+    
+    return variables
+
+def checkLineForKeyValue(line):
+    '''
+    checks if the line has the correct length. Then returns edited list.
+    '''
+
+    length = len(line) 
+    if length >= 2 and line[1] == '=':
+        return True,line[0],line[2]
+    else:
+        return False,"error","error"
+    
 
 
-file = open('test.txt',mode='w')
-file.write("")
-file.close()
+def stringToValue(value):
+    '''
+    Transforms a string into a:
+    1. integer, 2. float, 3. list
+    else: does nothing
+    '''
 
-file = open('test.txt',mode='a')
-for i in range(0,3):
-    file.write(f"{i}: hello!\n")
-file.close()
+    if value.isdigit():
+        return int(value)
+    elif is_float(value):
+        return float(value)
+    elif is_list(value):
+        return listify(value)
+    else:
+        return value
 
-file = open('test.txt',mode='r')
-print(file.read())
-file.close()
+
+def is_float(word):
+    '''
+    Checks if the string is a float
+    '''
+    try:
+        float(word)
+        return True
+    except ValueError:
+        return False
+
+def is_list(word):
+    '''
+    Checks if the string is a list
+    '''
+    return word[0] == "[" and word[-1] == "]"
+
+def listify(word):
+    '''
+    Turns a string into a list
+    '''
+    word = word[1:len(word)-1]
+
+    word = word.split(",")
+    newList = []
+    for part in word:
+        newList.append(stringToValue(part))
+    return newList
+
+
+if __name__  == "__main__":
+    '''
+    if run it will turn the inputted file into a dictionary of key:values.
+    Supported Values:
+     - Integers
+     - Floats
+     - Lists
+     - All othres will become strings
+    
+    '''
+    #gets user input for file name
+
+    #sets the file to variable
+    file = open('test.txt',mode='r')
+    text = file.read()
+    file.close()
+
+    #turns text into list based on each line
+    lineList = text.split('\n')
+
+    #turns text into list of lists. Line is 1st Dim. Each word is 2nd Dim.
+    wordList = []
+    for line in lineList:
+        wordList.append(line.split(' '))
+
+    #Creates the dictionary.
+    values = setVars(wordList)
+    print(values)
+
+
+
+
+
+
+
