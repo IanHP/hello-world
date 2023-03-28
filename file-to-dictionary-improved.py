b9e637030e2791ff54d49103abe0e793b72bf25a
IanHP <ian.herzogpohl@gmail.com>
2023-03-28 13:16:37 -0400

    created file-dictionary-imp file

---
+++ file-to-dictionary-improved.py
@@ -0,0 +1,146 @@
+"""
+Steps:
+1. Read the file and turn it into 1 massive string
+2. split into different lists (each index = 1 k:v pair)
+3. Split each list into another list (list of list), seperating key and value
+4. Ensure that the key and value have correct syntax, no repeats, ect.
+5. Tern value into correct form
+- if the value is an, or object, then run 2+ on it
+- if the value is a list (or similar), run it in a way, so the syntax of everything inside is also saved (modified 2+)
+"""
+
+def setVars(wordList):
+    '''
+    puts data from a list of lists (where [i][0] = name and [i][2] = value, and each [i] is a new key:value) into dictionary
+    '''
+
+    variables = {}
+
+    for index,line in enumerate(wordList, 1):
+        '''
+        runs as each list in the list of lists
+        '''
+
+        #checks if list is formatted properly. If yes returns the key and value.
+        check,key,value = checkLineForKeyValue(index,line)
+
+        #checks if key is already in use
+        keyList = variables.keys()
+
+        if key in keyList:
+            check = False
+            print(f'Duplicate key on line {index} (key: {key} || value: {value})')
+
+        #if previous checks are successful then add key, and edited value to dictionary
+        if check:
+            #then turns value into correct form.
+            value = stringToValue(value)
+
+            #adds key:value to dictionary
+            variables[key] = value
+
+
+    return variables
+
+def checkLineForKeyValue(index,line):
+    '''
+    checks if the line has the correct syntax. Then returns edited list.
+    '''
+
+    length = len(line) 
+    if length >= 2 and line[1] == '=':
+        return True,line[0],line[2]
+    else:
+        print(f'Improperly formatted key:value pair on line {index} ({line})')
+        return False,"error","error"
+    
+
+
+def stringToValue(value):
+    '''
+    Transforms a string into a:
+    1. integer, 2. float, 3. list
+    else: does nothing
+    '''
+
+    if value.isdigit():
+        return int(value)
+    elif is_float(value):
+        return float(value)
+    elif is_list(value):
+        return listify(value)
+    else:
+        return value
+
+
+def is_float(word):
+    '''
+    Checks if the string is a float
+    '''
+    try:
+        float(word)
+        return True
+    except ValueError:
+        return False
+
+def is_list(word):
+    '''
+    Checks if the string is a list
+    '''
+    return word[0] == "[" and word[-1] == "]"
+
+def listify(word):
+    '''
+    Turns a string into a list
+    '''
+    word = word[1:len(word)-1]
+
+    word = word.split(",")
+    newList = []
+    for part in word:
+        newList.append(stringToValue(part))
+    return newList
+
+def textDocToDictionary(fileLocation = 'N/A'):
+    '''
+    if run it will turn the inputted file into a dictionary of key:values.
+    Supported Values:
+     - Integers
+     - Floats
+     - Lists
+     - All othres will become strings
+    
+    '''
+
+    #gets user input for file name
+    if fileLocation == 'N/A':
+        print('What is the file named? If it is not in the same folder, please include the necessary directory info. \nenter here: ')
+        fileLocation = input()
+
+    #sets the file to a variable
+    file = open(fileLocation,mode='r')
+    text = file.read()
+    file.close()
+
+    #turns text into list based on each line
+    lineList = text.split('\n')
+
+    #turns text into list of lists. Line is 1st Dim. Each word is 2nd Dim.
+    wordList = []
+    for line in lineList:
+        wordList.append(line.split(' '))
+
+    #Creates the dictionary.
+    values = setVars(wordList)
+    print(values)
+
+
+
+if __name__  == "__main__":
+    textDocToDictionary('test.txt')
+
+
+
+
+
+
