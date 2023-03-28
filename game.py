8b03afc0463e3af48360b98b6916fc0753779192
IanHP <ian.herzogpohl@gmail.com>
2023-03-28 10:44:50 -0400

    adds in hello game

--- game.py
+++ game.py
@@ -4,9 +4,15 @@ This is a super awesome little game!!!!!
 
 
 
-file = open('test.txt',mode='w+')
+file = open('test.txt',mode='w')
+file.write("")
+file.close()
 
+file = open('test.txt',mode='a')
+for i in range(0,3):
+    file.write(f"{i}: hello!\n")
+file.close()
 
-file.write("hello")
+file = open('test.txt',mode='r')
 print(file.read())
 file.close()
